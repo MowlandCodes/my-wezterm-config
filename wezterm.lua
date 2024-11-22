26ef0b41dd6ae83d63d105bc9bb237168921d186
MowlandCodes <mowlandgaming@gmail.com>
2024-11-22 19:58:22 +0700

    GPU configured

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-local mux = wezterm.mux
+local gpus = wezterm.gui.enumerate_gpus()
 
 -- This will hold the configuration.
 local act = wezterm.action
@@ -12,8 +11,8 @@ local scheme = "Catppuccin Macchiato"
 config.color_scheme = scheme
 
 config.inactive_pane_hsb = {
-    saturation = 0.8,
-    brightness = 0.8,
+	saturation = 0.8,
+	brightness = 0.8,
 }
 
 config.command_palette_font_size = 12
@@ -21,10 +20,10 @@ config.command_palette_rows = 7
 
 -- Window styles
 config.window_padding = {
-    left = 5,
-    right = 5,
-    top = 5,
-    bottom = 5,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
 }
 
 config.scrollback_lines = 100000
@@ -40,12 +39,13 @@ config.show_tab_index_in_tab_bar = true
 config.window_close_confirmation = "NeverPrompt"
 config.unicode_version = 15
 config.window_background_opacity = 0.85
+config.webgpu_preferred_adapter = gpus[0]
 config.front_end = "WebGpu"
 
 -- Font Styles
 config.font = font("JetBrainsMonoNL Nerd Font")
 config.harfbuzz_features = {
-    "liga = 1",
+	"liga = 1",
 }
 config.freetype_load_flags = "NO_HINTING"
 config.font_size = 14
@@ -73,101 +73,103 @@ config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 2000 }
 
 config.keys = {
 
-    ---------------------- Window Management ----------------------
-    { mods = "CTRL|SHIFT",     key = "p",        action = act.ActivateCommandPalette },
-    -- Open new Wezterm Window
-    {
-        mods = "LEADER",
-        key = "n",
-        action = act.SpawnCommandInNewWindow({ args = { "pwsh.exe -nologo" }, cwd = default_cwd }),
-    },
-    { mods = "LEADER|SHIFT",   key = "Q",        action = act.QuitApplication },
-    -- Scrolling using Shortcut
-    { mods = "SHIFT",          key = "PageUp",   action = act.ScrollByPage(-0.25) },
-    { mods = "SHIFT",          key = "PageDown", action = act.ScrollByPage(0.25) },
+	---------------------- Window Management ----------------------
+	{ mods = "CTRL|SHIFT", key = "p", action = act.ActivateCommandPalette },
+	-- Open new Wezterm Window
+	{
+		mods = "LEADER",
+		key = "n",
+		action = act.SpawnCommandInNewWindow({ args = { "pwsh.exe -nologo" }, cwd = default_cwd }),
+	},
+	{ mods = "LEADER|SHIFT", key = "Q", action = act.QuitApplication },
+	-- Scrolling using Shortcut
+	{ mods = "SHIFT", key = "PageUp", action = act.ScrollByPage(-0.25) },
+	{ mods = "SHIFT", key = "PageDown", action = act.ScrollByPage(0.25) },
 
-    ---------------------- Editing Text ----------------------
-    { mods = "CTRL|SHIFT",     key = "c",        action = act.CopyTo("Clipboard") },
-    { mods = "CTRL|SHIFT",     key = "v",        action = act.PasteFrom("Clipboard") },
-    { mods = "CTRL|SHIFT|ALT", key = "c",        action = act.ActivateCopyMode },
-    { mods = "ALT",            key = "Escape",   action = act.CopyMode("Close") },
+	---------------------- Editing Text ----------------------
+	{ mods = "CTRL|SHIFT", key = "c", action = act.CopyTo("Clipboard") },
+	{ mods = "CTRL|SHIFT", key = "v", action = act.PasteFrom("Clipboard") },
+	{ mods = "CTRL|SHIFT|ALT", key = "c", action = act.ActivateCopyMode },
+	{ mods = "ALT", key = "Escape", action = act.CopyMode("Close") },
 
-    ---------------------- Tab Management ----------------------
-    -- Renaming Tab
-    {
-        mods = "LEADER",
-        key = "r",
-        action = act.PromptInputLine({
-            description = "Rename your Tab",
-            action = wezterm.action_callback(function(window, pane, line)
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end),
-        }),
-    },
-    {
-        mods = "LEADER",
-        key = "t",
-        action = act.SpawnCommandInNewTab({ args = { "pwsh.exe -nologo" }, cwd = default_cwd }),
-    },
+	---------------------- Tab Management ----------------------
+	-- Renaming Tab
+	{
+		mods = "LEADER",
+		key = "r",
+		action = act.PromptInputLine({
+			description = "Rename your Tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		mods = "LEADER",
+		key = "t",
+		action = act.SpawnCommandInNewTab({ args = { "pwsh.exe -nologo" }, cwd = default_cwd }),
+	},
 
-    -- Navigate through Tabs Relatively
-    { mods = "ALT",            key = "LeftArrow",  action = act.ActivateTabRelative(-1) },
-    { mods = "ALT",            key = "RightArrow", action = act.ActivateTabRelative(1) },
-    -- Close current active tab
-    { mods = "CTRL",           key = "w",          action = act.CloseCurrentTab({ confirm = true }) },
+	-- Navigate through Tabs Relatively
+	{ mods = "ALT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+	{ mods = "ALT", key = "RightArrow", action = act.ActivateTabRelative(1) },
+	-- Close current active tab
+	{ mods = "CTRL", key = "w", action = act.CloseCurrentTab({ confirm = true }) },
 
-    ---------------------- Font Management ----------------------
-    { mods = "CTRL",           key = "=",          action = act.IncreaseFontSize },
-    { mods = "CTRL",           key = "-",          action = act.DecreaseFontSize },
-    { mods = "LEADER|CTRL",    key = "0",          action = act.ResetFontSize },
+	---------------------- Font Management ----------------------
+	{ mods = "CTRL", key = "=", action = act.IncreaseFontSize },
+	{ mods = "CTRL", key = "-", action = act.DecreaseFontSize },
+	{ mods = "LEADER|CTRL", key = "0", action = act.ResetFontSize },
 
-    ---------------------- Pane / Multiplexer Management ----------------------
-    { mods = "LEADER|SHIFT",   key = "|",          action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { mods = "LEADER",         key = "\\",         action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { mods = "CTRL|ALT|SHIFT", key = "|",          action = act.PaneSelect({ alphabet = "0123456789" }) },
-    { mods = "LEADER|SHIFT",   key = "X",          action = act.CloseCurrentPane({ confirm = false }) },
-    { mods = "ALT",            key = "Enter",      action = act.ToggleFullScreen },
-    { mods = "ALT",            key = "h",          action = act.Hide },
-    { mods = "CTRL|SHIFT",     key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
-    { mods = "CTRL|SHIFT",     key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
-    { mods = "CTRL|SHIFT",     key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
-    { mods = "CTRL|SHIFT",     key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+	---------------------- Pane / Multiplexer Management ----------------------
+	{ mods = "LEADER|SHIFT", key = "|", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "\\", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ mods = "CTRL|ALT|SHIFT", key = "|", action = act.PaneSelect({ alphabet = "0123456789" }) },
+	{ mods = "LEADER|SHIFT", key = "X", action = act.CloseCurrentPane({ confirm = false }) },
+	{ mods = "ALT", key = "Enter", action = act.ToggleFullScreen },
+	{ mods = "ALT", key = "h", action = act.Hide },
+	{ mods = "CTRL|SHIFT", key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+	{ mods = "CTRL|SHIFT", key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+	{ mods = "CTRL|SHIFT", key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+	{ mods = "CTRL|SHIFT", key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
 }
 
 -- For navigate through tabs based on Index
 
 for i = 0, 9 do
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = wezterm.action.ActivateTab(i),
-    })
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
 end
 
-
 wezterm.on("update-right-status", function(window, _)
-    local SOLID_LEFT_ARROW = ""
-    local ARROW_FOREGROUND = { Foreground = { Color = "#C6A0F6" } }
-    local prefix = ""
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#C6A0F6" } }
+	local prefix = ""
 
-    if window:leader_is_active() then
-        prefix = " " .. utf8.char(0x1f30a)
-        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-    end
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a)
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
 
-    if window:active_tab():tab_id() ~= 0 then
-        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-    end
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end
 
-    window:set_left_status(wezterm.format {
-        { Background = { Color = "#b7bdf8" } },
-        { Text = prefix },
-        ARROW_FOREGROUND,
-        { Text = SOLID_LEFT_ARROW }
-    })
+	if window:active_tab():tab_id() == 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#C6A0F6" } }
+	end
 
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
 end)
 
 -----------------------------------------------------------------------------------
